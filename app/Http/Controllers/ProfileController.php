034a98392ba6005d23b4077cb882b0498032b76c
baxromjon <gitbahromjon77@gmail.com>
2024-03-11 14:56:28 +0300

    yangi o'zgarishlar kiritildi

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,6 +14,7 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
+
     public function edit(Request $request): View
     {
         return view('profile.edit', [
@@ -24,6 +25,7 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
+
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
@@ -40,6 +42,9 @@ class ProfileController extends Controller
     /**
      * Delete the user's account.
      */
+ /**
+  *Delete the user's account.
+  */
     public function destroy(Request $request): RedirectResponse
     {
         $request->validateWithBag('userDeletion', [
@@ -54,6 +59,7 @@ class ProfileController extends Controller
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+
 
         return Redirect::to('/');
     }
